9a6422d1d5dfbd25a663ca4a21f4b959929e6c5a
Marcial Cahuaya <cahuaya.marcial@gmail.com>
2019-04-17 12:09:18 -0400

    Variables y constantes

---
+++ 1.1. Variables y constantes/1.1.1. variables.py
@@ -0,0 +1,6 @@
+## A continuación, se creará un par de variables a modo de ejemplo. Una de tipo cadenas de caracteres y una de tipo entero:
+saludo = "Hola Mundo" # cadenas de caracteres
+print(saludo)
+numero = 21
+print(numero)
+## Como puede ver en Python, a diferencia de muchos otros lenguajes, no se declara el tipo de la variable al crearla.
---
+++ 1.1. Variables y constantes/1.1.2. constantes.py
@@ -0,0 +1,8 @@
+## las constantes son escritas en letras MAYÚSCULAS y separadas las palabras con el carácter underscore _. 
+## A continuación, se presentan algunos ejemplos del uso de constantes:
+
+IP_DB_SERVER = "127.0.0.1"
+PORT_DB_SERVER = 3307
+USER_DB_SERVER = "root"
+PASSWORD_DB_SERVER = "123456"
+DB_NAME = "db_example"
